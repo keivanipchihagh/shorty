96385562bab6a033ce1b57016cd23d39778194f2
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2025-03-28 21:17:00 +0330

    feat: Add logrus

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/keivanipchihagh/shorty/internal"
 	"github.com/keivanipchihagh/shorty/internal/configs"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	config := configs.NewConfig()
+	log.SetFormatter(&log.JSONFormatter{})
 	internal.Start(config)
 }
